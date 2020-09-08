22ccf7e86e7b99ab1921dd82c3f1418eed4b53b6
Heyy Dude Tech <heyydudetech@gmail.com>
2020-09-08 16:01:36 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How old is Java?',
+        a: '10',
+        b: '17',
+        c: '25',
+        d: '110',
+        correct: 'c'
+    }, {
+        question: 'What is the most used programming language in 2019?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'd'
+    }, {
+        question: 'Who is the President of USA?',
+        a: 'Joe Biden',
+        b: 'Donald Trump',
+        c: 'Hilary Clinton',
+        d: 'Barack Obama',
+        correct: 'b'
+    }, {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheets',
+        c: 'Jason Object Notation',
+        d: 'Helicopters Terminals Motorboats Lamborghinis',
+        correct: 'a'
+    }, {
+        question: 'What year was JavaScript launched?',
+        a: '1996',
+        b: '1995',
+        c: '1994',
+        d: 'none of the above',
+        correct: 'b'
+    }
+]
