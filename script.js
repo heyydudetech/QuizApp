6d0fc5e29e35986ce39e6961c160a4927a9f85ba
Heyy Dude Tech <heyydudetech@gmail.com>
2020-09-09 12:00:44 +0200

    final javascript

--- script.js
+++ script.js
@@ -35,4 +35,71 @@ const quizData = [
         d: 'none of the above',
         correct: 'b'
     }
-]
+];
+
+const quiz = document.getElementById("quiz");
+const answerElements = document.querySelectorAll(".answer");
+const questionElement = document.getElementById("question");
+
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+    questionElement.innerText = currentQuizData.question;
+
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerElements.forEach((answerEle) => {
+        if (answerEle.checked) {
+            answer = answerEle.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerElements.forEach((answerEle) => {
+        answerEle.checked = false;
+    });
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions.</h2> <button onclick="location.reload()">Reload</button>`;
+        }
+    }
+
+
+
+});
